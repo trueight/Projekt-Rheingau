bea9b54f456fa55117f02dcfe40e976555216e17
Hachi-R <drive8hachi@gmail.com>
2024-09-27 01:14:06 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
+ </script>
+
+<svelte:head> 
+  <link rel="preload" as="font" href="/fonts/geist/GeistVF.woff2" type="font/woff2" crossorigin="anonymous"> 
+  <link rel="preload" as="font" href="/fonts/geist/GeistMonoVF.woff2" type="font/woff2" crossorigin="anonymous"> 
+</svelte:head>
+	
+ <ModeWatcher />
+ <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import HeroiconsCog6ToothSolid from '~icons/heroicons/cog-6-tooth-solid';
+  import { Button } from "$lib/components/ui/button";
+ </script>
+  
+  icon
+ <Button variant="outline" size="icon">
+  <HeroiconsCog6ToothSolid />
+ </Button>
